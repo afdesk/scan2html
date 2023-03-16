a3ee9e8fb44fc65880e327740756f0ee478ac1df
maksim <maksim.malikov@smartforce.io>
2023-03-16 14:49:48 +0600

    feat: support the report for secret scan
    render refactor, minor changes

--- src/js/render.js
+++ src/js/render.js
@@ -1,135 +1,252 @@
-function getResultTablesByResults(tableTemplate, emptyVulnsTemplate, tableRowTemplate, vulnLinkTemplate, resultsData) {
-	return resultsData.map(result => {
-		const resultTable = tableTemplate.content.cloneNode(true);
-		const resultTableHeader = resultTable.querySelector('.header__title');
-		const resultTableBody = resultTable.querySelector('tbody');
-		if (!result.Vulnerabilities || !result.Vulnerabilities.length) {
-			const emptyVulns = emptyVulnsTemplate.content.cloneNode(true);
-			resultTableBody.append(emptyVulns);
-			return resultTable;
-		}
-		/*filling table*/
-		result.Vulnerabilities.forEach(vuln => {
-			const resultTableRow = tableRowTemplate.content.cloneNode(true);
-			const pkgName = resultTableRow.querySelector('.pkg-name');
-			pkgName.textContent = vuln.PkgName;
-			const vulnerability = resultTableRow.querySelector('.vuln');
-			vulnerability.textContent = vuln.VulnerabilityID;
-			const severity = resultTableRow.querySelector('.severity');
-			severity.textContent = vuln.Severity;
-			const pkgVersion = resultTableRow.querySelector('.pkg-version');
-			pkgVersion.textContent = vuln.InstalledVersion;
-			const fixVersion = resultTableRow.querySelector('.fix-version');
-			fixVersion.textContent = vuln.FixedVersion;
-			const links = resultTableRow.querySelector('.links');
-			vuln.References && vuln.References.forEach(ref => {
-				const vulnLink = vulnLinkTemplate.content.cloneNode(true);
-				const linkElement = vulnLink.querySelector('a');
-				linkElement.href = ref;
-				linkElement.textContent = ref;
-				links.append(linkElement);
-			})
-			resultTableBody.append(resultTableRow);
-		})
-		/*filling table*/
-		resultTableHeader.textContent = result.Type;
-		return resultTable;
-	});
+/* available templates */
+const reportTitleTemplate = document.querySelector("#report-title");
+const tableTemplate = document.querySelector("#table");
+const tableRowTemplate = document.querySelector("#table-row");
+const vulnLinkTemplate = document.querySelector("#vuln-link");
+const emptyVulnsTemplate = document.querySelector("#empty-vuln-header");
+const topVulnTemplate = document.querySelector("#top-vuln");
+const miscTableRowTemplate = document.querySelector("#misc-table-row");
+const mistTableTemplate = document.querySelector("#misc-table");
+const emptyMiscTemplate = document.querySelector("#empty-misc-header");
+const topMiscTemplate = document.querySelector("#top-misc");
+const secretResultsTemplate = document.querySelector("#secret-results");
+const secretTemplate = document.querySelector("#secret");
+const secretCodeLineTemplate = document.querySelector("#secret__code-line");
+
+/* available templates */
+
+function fillResultTable(result) {
+  const resultTable = tableTemplate.content.cloneNode(true);
+  const resultTableHeader = resultTable.querySelector(".header__title");
+  const resultTableBody = resultTable.querySelector("tbody");
+  result.Vulnerabilities.forEach((vuln) => {
+    const resultTableRow = tableRowTemplate.content.cloneNode(true);
+    const pkgName = resultTableRow.querySelector(".pkg-name");
+    pkgName.textContent = vuln.PkgName;
+    const vulnerability = resultTableRow.querySelector(".vuln");
+    vulnerability.textContent = vuln.VulnerabilityID;
+    const severity = resultTableRow.querySelector(".severity");
+    severity.textContent = vuln.Severity;
+    const pkgVersion = resultTableRow.querySelector(".pkg-version");
+    pkgVersion.textContent = vuln.InstalledVersion;
+    const fixVersion = resultTableRow.querySelector(".fix-version");
+    fixVersion.textContent = vuln.FixedVersion;
+    const links = resultTableRow.querySelector(".links");
+    if (vuln.References) {
+      vuln.References.forEach((ref) => {
+        const vulnLink = vulnLinkTemplate.content.cloneNode(true);
+        const linkElement = vulnLink.querySelector("a");
+        linkElement.href = ref;
+        linkElement.textContent = ref;
+        links.append(linkElement);
+      });
+    }
+    resultTableBody.append(resultTableRow);
+  });
+  /*filling table*/
+  resultTableHeader.textContent = result.Type;
+  return resultTable;
 }
 
-function getMiscResultTables(topMisconfiguration, mistTableTemplate, emptyMiscTemplate, miscTableRowTemplate) {
-	return topMisconfiguration.Results.map(result => {
-		const resultTable = mistTableTemplate.content.cloneNode(true);
-		const resultTableHeader = resultTable.querySelector('.header__title');
-		const resultTableBody = resultTable.querySelector('tbody');
-		if (!result.Misconfigurations || !result.Misconfigurations.length) {
-			const emptyMisc = emptyMiscTemplate.content.cloneNode(true);
-			resultTableBody.append(emptyMisc);
-			return resultTable;
-		}
-		/*filling table*/
-		result.Misconfigurations.forEach(misc => {
-			const resultTableRow = miscTableRowTemplate.content.cloneNode(true);
-			const pkgName = resultTableRow.querySelector('.misc-type');
-			pkgName.textContent = misc.Type;
-			const vulnerability = resultTableRow.querySelector('.misc-id');
-			vulnerability.textContent = misc.ID;
-			const severity = resultTableRow.querySelector('.misc-check');
-			severity.textContent = misc.Title;
-			const pkgVersion = resultTableRow.querySelector('.misc-sev');
-			pkgVersion.textContent = misc.Severity;
-			const fixVersion = resultTableRow.querySelector('.misc-msg');
-			fixVersion.textContent = misc.Message;
+function fillSecrets(result) {
+  const secretResults = secretResultsTemplate.content.cloneNode(true);
+  const secretsContainer = secretResults.querySelector(
+    ".secret-results__content"
+  );
+  const secretsContainerTitle = secretResults.querySelector(
+    ".secret-results__title"
+  );
+  const secretResultsTitleTarget = secretsContainerTitle.querySelector(
+    ".secret-results__title-target"
+  );
+  secretResultsTitleTarget.textContent = result.Target;
+  const secretResultsTitleClass = secretsContainerTitle.querySelector(
+    ".secret-results__title-class"
+  );
+  secretResultsTitleClass.textContent = `(${result.Class})`;
+  // Secrets field is not empty
+  const secrets = result.Secrets.map((secret) => {
+    const secretNode = secretTemplate.content.cloneNode(true);
+    const secretTitle = secretNode.querySelector(".secret__title");
+    const secretHead = secretNode.querySelector(".secret__head");
+    const secretSrc = secretNode.querySelector(".secret__src");
+    const secretCode = secretNode.querySelector(".secret__code");
+    /*HEAD*/
+    const secretSeverity = secretHead.querySelector(".secret__severity");
+    secretSeverity.classList.toggle(`severity-${secret.Severity}`);
+    secretSeverity.textContent = secret.Severity;
+    const secretType = secretHead.querySelector(".secret__type");
+    secretType.textContent = `${secret.Category} (${secret.RuleID})`;
+    /*HEAD*/
+    /*TITLE*/
+    secretTitle.textContent = secret.Title;
+    /*TITLE*/
+    /*SRC*/
+    const secretSrcFile = secretSrc.querySelector(".secret__src-file");
+    secretSrcFile.textContent = result.Target;
+    const secretSrcLines = secretSrc.querySelector(".secret__src-lines");
+    secretSrcLines.textContent =
+      secret.StartLine === secret.EndLine
+        ? secret.StartLine
+        : `${secret.StartLine} - ${secret.EndLine}`;
+    /*SRC*/
+    const lines = secret.Code.Lines.map((line) => {
+      const secretCodeLine = secretCodeLineTemplate.content.cloneNode(true);
+      const secretLine = secretCodeLine.querySelector(".secret__line");
+      const secretLineNumber = secretLine.querySelector(".secret__line-number");
+      secretLineNumber.textContent = line.Number.toString();
+      const secretLineCode = secretLine.querySelector(".secret__line-code pre");
+      secretLineCode.textContent = line.Content;
+      if (line.IsCause) {
+        secretLine.classList.toggle("secret__line-cause");
+      }
+      return secretCodeLine;
+    });
+    secretCode.append(...lines);
+    return secretNode;
+  });
+  secretsContainer.append(...secrets);
+  return secretResults;
+}
 
-			resultTableBody.append(resultTableRow);
-		})
-		/*filling table*/
-		resultTableHeader.textContent = result.Type;
-		return resultTable;
-	});
+function getEmptyResult() {
+  const resultTable = tableTemplate.content.cloneNode(true);
+  const resultTableBody = resultTable.querySelector("tbody");
+  const emptyVulns = emptyVulnsTemplate.content.cloneNode(true);
+  resultTableBody.append(emptyVulns);
+  return resultTable;
+}
+
+function getResultTablesByResults(resultsData) {
+  const resultTables = [];
+  const secretsTables = [];
+  for (const result of resultsData) {
+    if (result.Vulnerabilities && result.Vulnerabilities.length) {
+      // Vulnerabilities field is not empty
+      const resultTable = fillResultTable(result);
+      resultTables.push(resultTable);
+    } else if (result.Secrets && result.Secrets.length) {
+      // Secrets field is not empty
+      const secret = fillSecrets(result);
+      secretsTables.push(secret);
+    } else {
+      // on empty/unhandled
+      const emptyResult = getEmptyResult();
+      resultTables.push(emptyResult);
+    }
+  }
+  return { resultTables, secretsTables };
+}
+
+function getMiscResultTables(topMisconfiguration) {
+  return topMisconfiguration.Results.map((result) => {
+    const resultTable = mistTableTemplate.content.cloneNode(true);
+    const resultTableHeader = resultTable.querySelector(".header__title");
+    const resultTableBody = resultTable.querySelector("tbody");
+    if (!result.Misconfigurations || !result.Misconfigurations.length) {
+      const emptyMisc = emptyMiscTemplate.content.cloneNode(true);
+      resultTableBody.append(emptyMisc);
+      return resultTable;
+    }
+    /*filling table*/
+    result.Misconfigurations.forEach((misc) => {
+      const resultTableRow = miscTableRowTemplate.content.cloneNode(true);
+      const pkgName = resultTableRow.querySelector(".misc-type");
+      pkgName.textContent = misc.Type;
+      const vulnerability = resultTableRow.querySelector(".misc-id");
+      vulnerability.textContent = misc.ID;
+      const severity = resultTableRow.querySelector(".misc-check");
+      severity.textContent = misc.Title;
+      const pkgVersion = resultTableRow.querySelector(".misc-sev");
+      pkgVersion.textContent = misc.Severity;
+      const fixVersion = resultTableRow.querySelector(".misc-msg");
+      fixVersion.textContent = misc.Message;
+
+      resultTableBody.append(resultTableRow);
+    });
+    /*filling table*/
+    resultTableHeader.textContent = result.Type;
+    return resultTable;
+  });
 }
 
 function renderCreatedAt() {
-	if (!createdAt) {
-		return console.error("Creation timestamp not loaded");
-	}
-	const titleTimeElem = document.querySelector('#title-time');
-	titleTimeElem.innerHTML = new Date(createdAt).toLocaleString();
-	document.title += ' ' + titleTimeElem.innerHTML;
+  if (!createdAt) {
+    return console.error("Creation timestamp not loaded");
+  }
+  const titleTimeElem = document.querySelector("#title-time");
+  titleTimeElem.innerHTML = new Date(createdAt).toLocaleString();
+  document.title += " " + titleTimeElem.innerHTML;
+}
+
+function getReportTitle(trivyData) {
+  if (trivyData.Results) {
+    return trivyData.Results[0].Target;
+  }
+  if (trivyData.Vulnerabilities) {
+    return trivyData.Vulnerabilities[0].Name;
+  }
+}
+
+function getTopMiscResultTables() {
+  return trivyData.Misconfigurations.map((topMisconfiguration) => {
+    const topMisc = topMiscTemplate.content.cloneNode(true);
+    const topMiscTablesContainer = topMisc.querySelector(".tables-container");
+    const topMiscHeader = topMisc.querySelector("h2");
+    topMiscHeader.textContent = `${topMisconfiguration.Name} (${topMisconfiguration.Kind})`;
+    const resultTables = getMiscResultTables(
+      topMisconfiguration,
+      mistTableTemplate,
+      emptyMiscTemplate,
+      miscTableRowTemplate
+    );
+    topMiscTablesContainer.append(...resultTables);
+    return topMisc;
+  });
+}
+
+function getTopVulnerabilities() {
+  return trivyData.Vulnerabilities.map((topVulnerability) => {
+    const topVuln = topVulnTemplate.content.cloneNode(true);
+    const topVulnTablesContainer = topVuln.querySelector(".tables-container");
+    const topVulnHeader = topVuln.querySelector("h2");
+    topVulnHeader.textContent = `${topVulnerability.Name} (${topVulnerability.Kind})`;
+
+    const { resultTables } = getResultTablesByResults(topVulnerability.Results);
+    topVulnTablesContainer.append(...resultTables);
+    return topVuln;
+  });
 }
 
 function render() {
-	if (!trivyData) {
-		return console.error("Report data not loaded");
-	}
-	const root = document.querySelector('#root');
-	if (!root) {
-		return console.error("Can't find root");
-	}
-	/* available templates */
-	const reportTitleTemplate = document.querySelector('#report-title');
-	const tableTemplate = document.querySelector('#table');
-	const tableRowTemplate = document.querySelector('#table-row');
-	const vulnLinkTemplate = document.querySelector('#vuln-link');
-	const emptyVulnsTemplate = document.querySelector('#empty-vuln-header');
-	const topVulnTemplate = document.querySelector('#top-vuln');
-	const miscTableRowTemplate = document.querySelector('#misc-table-row');
-	const mistTableTemplate = document.querySelector('#misc-table');
-	const emptyMiscTemplate = document.querySelector('#empty-misc-header');
-	const topMiscTemplate = document.querySelector('#top-misc');
-	/* available templates */
+  if (!trivyData) {
+    return console.error("Report data not loaded");
+  }
+  const root = document.querySelector("#root");
+  if (!root) {
+    return console.error("Can't find root");
+  }
+  const reportTitleTarget = reportTitleTemplate.querySelector(
+    ".report-title__target"
+  );
+  reportTitleTarget.textContent = getReportTitle(trivyData);
 
-	const reportTitleTarget = reportTitleTemplate.querySelector('.report-title__target');
-	let resultTables;
-	if (trivyData.Results) {
-		reportTitleTarget.textContent = trivyData.Results[0].Target;
-		resultTables = getResultTablesByResults(tableTemplate, emptyVulnsTemplate, tableRowTemplate, vulnLinkTemplate, trivyData.Results);
-	} else if (trivyData.Vulnerabilities) {
-		reportTitleTarget.textContent = trivyData.Vulnerabilities[0].Name;
-		resultTables = trivyData.Vulnerabilities.map(topVulnerability => {
-			const topVuln = topVulnTemplate.content.cloneNode(true);
-			const topVulnTablesContainer = topVuln.querySelector('.tables-container');
-			const topVulnHeader = topVuln.querySelector('h2');
-			topVulnHeader.textContent = `${topVulnerability.Name} (${topVulnerability.Kind})`;
-			const resultTables = getResultTablesByResults(tableTemplate, emptyVulnsTemplate, tableRowTemplate, vulnLinkTemplate, topVulnerability.Results);
-			topVulnTablesContainer.append(...resultTables);
-			return topVuln;
-		});
-	}
-	root.append(...resultTables);
-	if (trivyData.Misconfigurations) {
-		const miscResultTables = trivyData.Misconfigurations.map(topMisconfiguration => {
-			const topMisc = topMiscTemplate.content.cloneNode(true);
-			const topMiscTablesContainer = topMisc.querySelector('.tables-container');
-			const topMiscHeader = topMisc.querySelector('h2');
-			topMiscHeader.textContent = `${topMisconfiguration.Name} (${topMisconfiguration.Kind})`;
-			const resultTables = getMiscResultTables(topMisconfiguration, mistTableTemplate, emptyMiscTemplate, miscTableRowTemplate);
-			topMiscTablesContainer.append(...resultTables);
-			return topMisc;
-		});
-		root.append(...miscResultTables);
-	}
-	renderCreatedAt()
+  if (trivyData.Results) {
+    const { resultTables, secretsTables } = getResultTablesByResults(
+      trivyData.Results
+    );
+    if (resultTables) root.append(...resultTables);
+    if (secretsTables) root.append(...secretsTables);
+  } else if (trivyData.Vulnerabilities) {
+    const topVulns = getTopVulnerabilities();
+    root.append(...topVulns);
+  }
+
+  if (trivyData.Misconfigurations) {
+    const miscResultTables = getTopMiscResultTables();
+    root.append(...miscResultTables);
+  }
+  renderCreatedAt();
 }
 
-document.addEventListener('DOMContentLoaded', render)
+document.addEventListener("DOMContentLoaded", render);
