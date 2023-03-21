eedad1a330437107adc5e503595e31cc04cf7a3d
maksim <maksim.malikov@smartforce.io>
2023-03-21 15:14:14 +0600

    build: add build-dev option

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,31 @@ const gulp = require("gulp");
 const cleanCSS = require("gulp-clean-css");
 const inline = require("gulp-inline");
 const uglify = require("gulp-uglify");
-const htmlSplit = require('gulp-htmlsplit');
+const htmlSplit = require("gulp-htmlsplit");
 
 function makeInline() {
-	return gulp
-		.src("./src/index.html")
-		.pipe(
-			inline({
-				base: "./src",
-				js: uglify,
-				css: function () {
-					return cleanCSS();
-				},
-			})
-		);
+  return gulp.src("./src/index.html").pipe(
+    inline({
+      base: "./src",
+      js: uglify,
+      css: function () {
+        return cleanCSS();
+      },
+    })
+  );
 }
 
-gulp.task('default', function () {
-	return makeInline()
-		.pipe(htmlSplit())
-		.pipe(gulp.dest("./dist"));
-})
+gulp.task("default", function () {
+  return makeInline().pipe(htmlSplit()).pipe(gulp.dest("./dist"));
+});
 
+gulp.task("dev", function () {
+  return gulp
+    .src("./src/index.html")
+    .pipe(htmlSplit())
+    .pipe(gulp.dest("./dist"));
+});
 
-gulp.task('single', function () {
-	return makeInline()
-		.pipe(gulp.dest("./dist"));
-})
+gulp.task("single", function () {
+  return makeInline().pipe(gulp.dest("./dist"));
+});
