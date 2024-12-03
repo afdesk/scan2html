26712bf2d04a682b3aac17360e5ffdcea772b80b
olsova <olga.solopova@smartforce.io>
2024-12-03 17:57:31 +0600

    feat: add k8s scan report

--- render/render.go
+++ render/render.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"text/template"
 
+	k8s "github.com/aquasecurity/trivy/pkg/k8s/report"
 	"github.com/aquasecurity/trivy/pkg/types"
 	"golang.org/x/xerrors"
 )
@@ -14,9 +15,20 @@ import (
 var htmlTmpl []byte
 
 func Render(fileName string, inputData []byte) error {
+	var kubernetes k8s.Report
 	var report types.Report
+
+	if err := json.Unmarshal(inputData, &kubernetes); err != nil {
+		return xerrors.Errorf("error decoding body: %v\n", err)
+	}
+
 	if err := json.Unmarshal(inputData, &report); err != nil {
 		return xerrors.Errorf("error decoding body: %v\n", err)
+	}
+
+	results := report.Results
+	for _, resource := range kubernetes.Resources {
+		results = append(results, resource.Results...)
 	}
 
 	tmpl, err := template.New("temp").Parse(string(htmlTmpl))
@@ -30,7 +42,7 @@ func Render(fileName string, inputData []byte) error {
 	}
 	defer output.Close()
 
-	if err := tmpl.Execute(output, report); err != nil {
+	if err = tmpl.Execute(output, results); err != nil {
 		return xerrors.Errorf("error executing template: %v\n", err)
 	}
 
